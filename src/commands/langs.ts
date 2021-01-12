3adee12645f895f67c52fd9543b6d45f10cb433c
derpyhooves <56843137+scottoss@users.noreply.github.com>
2021-01-12 18:26:38 +0100

    Update langs.ts

--- src/commands/langs.ts
+++ src/commands/langs.ts
@@ -1,1 +1,14 @@
 const { embed } = require('../common/langsEmbed');
+
+
+export default class LangsCommand implements Command {
+    name = 'langs';
+    summary = 'show all langs';
+    precondition: Permission = '';
+    cooldown = 0;
+    module = 'General';
+    
+    execute = async(ctx: CommandContext) => {
+        await ctx.channel.send(embed);
+    }
+}
